0d8f70284ac3f2739122f3e09a0d113c4ae128fb
Rick Huang <89106824+netapp-rickhuang-ai@users.noreply.github.com>
2025-04-16 16:43:59 -0700

    [NAS-285] added 3 benchmarks using 3 models
    CUDA.profiler
    10 randomized runs per avg_tokens_second [FINAL] results 
    Env: single Tesla T4 in Jupyter-KVM

---
+++ transformer_torch_mistral-7b-instruct-v0.3-bnb-4bit_profiler_sim.py
@@ -0,0 +1,228 @@
+from pathlib import Path
+import pandas as pd
+import torch
+import numpy
+from numpy import random
+
+from torch.profiler import profile, record_function, ProfilerActivity
+
+import json
+import sys
+sys.path.append('/home/ailab/files/llama_index')
+import llama_index
+import llama_index.core
+import llama_index.core.base.response.schema as response_schema
+import llama_index.readers.file as file_readers
+from llama_index.core import Document
+
+from llama_index.core.node_parser import (
+    HierarchicalNodeParser,
+    get_leaf_nodes,
+    get_root_nodes,
+)
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex
+from llama_index.core.retrievers import AutoMergingRetriever
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from vllm import LLM  # Use VLLM instead of ChatOllama
+from vllm import SamplingParams # for offline batch inference
+
+from llama_index.core.indices.list import ListIndex
+
+import langchain
+from langchain_core.runnables.base import RunnableSerializable
+# from llama_index.langchain import chat_models
+from langchain import chat_models
+from typing import Union
+
+
+# Check if CUDA is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load TXT document using file_readers
+file_loader = file_readers.RTFReader()
+txt_path = Path("/home/ailab/files/llama_index/data/llama2_paper/source_files/source.txt")
+docs0 = file_loader.load_data(input_file=txt_path)
+doc_text = "\n\n".join([d.get_content() for d in docs0])
+docs = [Document(text=doc_text)]
+print(f"\n[INFO] var docs is of type: ", type(docs))
+print(f"\n[INFO] txt file read INTO: docs, where 'doc_text' containing content = \n", doc_text)
+
+jsn_path = Path("/home/ailab/files/llama_index/data/yjoonjang-squad_v2_ragsoluted.json")
+with open(jsn_path, 'r') as file:
+    jsn_file = json.load(file)
+# Normalize JSON data
+df = pd.json_normalize(jsn_file)
+print(f"\n[DEUBG] var df is of type: ", type(df))
+
+# Save DataFrame to CSV
+csv_path = jsn_path.with_suffix('.csv')
+df.to_csv(csv_path, index=False)
+print(f"\n[INFO] JSON file converted to CSV and saved at {csv_path}")
+
+pd_csv_file_loader = file_readers.PandasCSVReader()
+print(f"\n[INFO] Pandas CSV read into DataFrame {df}")
+print(f"\n[INFO] df var currently of type: ", type(df))
+
+docs1 = pd_csv_file_loader.load_data(file=csv_path)
+docs1_text = "\n\n".join([d.get_content() for d in docs1]) # type: str
+docs_1 = [Document(text=docs1_text)]
+docs.append(docs_1) # docs = [docs[0], docs[1]] = [Document(text=doc_text), Document(text=doc1_text)]
+docs_attr_dct = {"_id",""}
+
+# Convert DataFrame to Document
+doc1_text = df.to_string(index=False)
+# Concatenate doc_text and doc1_text
+txt_csv_text = doc_text + "\n\n" + doc1_text
+docss = [Document(text=txt_csv_text)]
+
+# Parse nodes from documents
+node_parser = HierarchicalNodeParser.from_defaults()
+nodes = node_parser.get_nodes_from_documents(docss)
+print("\n[INFO] Text+CSV HierarchicalNodeParser: 'nodes' length =", len(nodes))
+
+# Get leaf and root nodes
+leaf_nodes = get_leaf_nodes(nodes)
+print("\n[INFO] No. of 'leaf' nodes that don't have children of their own =", len(leaf_nodes))
+root_nodes = get_root_nodes(nodes)
+
+# LLM batch offline inference
+generation_config="/home/ailab/files/mistral-7b-instruct-v0.3-bnb-4bit/config.json" 
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.5, top_p=0.95)
+print("\n[INFO] From VLLM: sampling_params is of type ", type(sampling_params))
+print("\n[INFO] From VLLM: sampling_params will be used for batch offline inference, where temperature=0.8, top_p=0.95")
+
+# Free up memory after adding nodes to the docstore
+import gc
+
+# Define storage context
+docstore = SimpleDocumentStore()
+docstore.add_documents(nodes)
+storage_context = StorageContext.from_defaults(docstore=docstore)
+# Build ListIndex
+base_index = ListIndex.from_documents(documents=docss)
+print("[INFO] ListIndex `base_index` built successfully.")
+# Delete unused variables
+del nodes, docss, doc_text, doc1_text, txt_csv_text, leaf_nodes, root_nodes
+
+# Run garbage collection to free system memory
+gc.collect()
+import torch.distributed as dist
+def cleanup():
+    if dist.is_initialized():
+        dist.destroy_process_group()
+    return "\n[INFO] torch.distributed.destroy_process_group() cleanup SUCCESSFUL."
+
+
+cleanup()
+torch.cuda.empty_cache()
+
+
+import accelerate
+# Load the tokenizer and model
+model_path = "/home/ailab/files/mistral-7b-instruct-v0.3-bnb-4bit"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
+
+# Move the model to the GPU
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+ 
+# Step 1: Tokenize the input & Encode the tokens
+query_str = "Who's Mark Zuckerberg? Describe his role in Meta in 500 words."
+# print(f"\n[DEBUG] for query: {query_str}, inputs = ", tokenizer.encode(query_str, skip_special_tokens=True))
+print(f"\n[INFO] for query: {query_str}, tokenized inputs = ", tokenizer.encode(query_str))
+
+inputs = tokenizer.encode(query_str, return_tensors="pt").to(device)
+print(f"\n[INFO] for {query_str}, tokenized inputs = ", inputs) # [NOTE] type(inputs) = tensor
+
+# Adjust sampling parameters for efficiency
+outputs = model.generate(inputs, max_length=1024, temperature=0.3, top_p=0.9, do_sample=True)
+model_resp = tokenizer.decode(outputs[0])
+print(f"\n[INFO] for query: {query_str}, the model {model.type} response with decoded outputs: ", model_resp) # type(outputs) = tensor
+
+encoded_input_tensors = tokenizer.encode(text=query_str, return_tensors='pt').to(device)
+"""
+tensor([[128000,  15546,    596,   4488,  67034,     30,  61885,    813,   3560,
+            304,  16197,    304,    220,   1135,   4339,     13]],
+       device='cuda:0')
+"""
+print(f"\n[INFO] for query: {query_str}, the encoded_input_tensors = ", encoded_input_tensors)
+
+cleanup()
+torch.cuda.empty_cache()
+
+
+tokens_per_second_results = []
+for i in range(10):
+    with torch.no_grad(): # for inferencing: Memory Efficiency, Speed Improvement, Safety
+        temp = random.rand()
+        print(f"\n[INFO] for the {i}-th run, set randomized temperature = ", temp)
+        # Define your chat template
+        chat_template = {
+            "role": "system",
+            "content": "You are a helpful, conversational, and professional assistant."
+        }
+        query_str = "Who's Manny Pacquio? Describe his career achievements in boxing in 500 words."
+        chat_messages = [
+            {"role": "user", "content": query_str}
+        ]
+        inputs = tokenizer(text=query_str, return_tensors="pt").to(device)
+        print(f"\n[INFO] for {query_str}, tokenized inputs tensors = ", inputs) # [NOTE] type(inputs) = tensor    
+        # Create CUDA events to measure time
+        # Measure the performance using PyTorch's built-in functions
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True) 
+        # Start CUDA profiler
+        torch.cuda.profiler.start()
+        # Record the start event
+        start_event.record()
+        # Adjust sampling parameters for efficiency
+        outputs = model.generate(**inputs, max_length=15436, temperature=temp, top_p=0.9, do_sample=True)
+        print(f"\n[INFO] for query: {query_str}, the model {model.type} response with outputs: (tensor)", outputs) # type(outputs) = tensor
+        # Record the end event
+        end_event.record()
+    
+        # Stop CUDA profiler
+        torch.cuda.profiler.stop()
+        
+        # Synchronize events
+        torch.cuda.synchronize()
+        
+        # Calculate the time taken in milliseconds
+        time_taken_ms = start_event.elapsed_time(end_event)
+        time_taken_s = time_taken_ms / 1000 # Convert milliseconds to seconds
+        # Calculate tokens per second
+        num_tokens = outputs.shape[1]
+        tokens_per_second = num_tokens / time_taken_s
+        tokens_per_second_results.append(tokens_per_second)
+        
+        # Print the results
+        print(f"\n[INFO] Tokens generated: {num_tokens}")
+        print(f"\n[INFO] Time taken: {time_taken_s:.4f} seconds")
+        print(f"\n[INFO] Tokens per second: {tokens_per_second:.2f}")
+            
+        # decoded_outputs = tokenizer.decode(outputs[0], skip_special_tokens=True) # type(decoded_outputs) = <class 'str'>
+        # print(f"\n[INFO] tokenizer decoded outputs: (text)", decoded_outputs)
+        # inputs_0 = tokenizer(text=decoded_outputs, return_tensors="pt").to(device)
+        # outputs_0 = model.generate(**inputs_0, max_new_tokens=8872, temperature=0.7, top_p=0.9, do_sample=True)
+        # print(f"\n[INFO] for query: {inputs_0}, the llm response was: ", tokenizer.decode(outputs_0[0], skip_special_tokens=True))
+        # Ensure cleanup is called at the end
+        torch.cuda.empty_cache()
+        cleanup()
+
+
+avg_tokens_per_second_results = sum(tokens_per_second_results) / len(tokens_per_second_results)
+print(f"\n[INFO][FINAL] Average tokens per second: {avg_tokens_per_second_results:.2f}")
+# Clear unused GPU memory
+torch.cuda.empty_cache()
+print("[INFO] System and GPU memory cleared after answering queries.")
